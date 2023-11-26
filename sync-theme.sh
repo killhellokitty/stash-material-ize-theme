8e30ea89b8f3f55898cf2e45e09a7c4d9b7cf3b8
Joshua Brown <killhellokitty21@gmail.com>
2023-11-25 20:35:10 -0600

    Sync Stash CSS

--- sync-theme.sh
+++ sync-theme.sh
@@ -2,4 +2,4 @@
 
 DIR_PATH=`dirname "$0"`
 
-fswatch -l 10 -o $HOME/stash-theme.css $HOME/stash-theme-Dracula.css $HOME/stash-theme_tabnavs.css | xargs -n10 zsh $DIR_PATH/commit_changes.sh
+fswatch -l 1 -o $HOME/stash-theme.css $HOME/stash-theme-Dracula.css $HOME/stash-theme_tabnavs.css | xargs -n1 zsh $DIR_PATH/commit_changes.sh
