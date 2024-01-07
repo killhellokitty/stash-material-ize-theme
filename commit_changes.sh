7189dcda614b8f4b5b25ad787a0a987ec092db32
Joshua Brown <killhellokitty21@gmail.com>
2024-01-07 12:49:30 -0600

    Sync Stash CSS

--- commit_changes.sh
+++ commit_changes.sh
@@ -2,10 +2,10 @@
 
 cd `dirname "$0"`
 
-cp $HOME/stash-theme.css .
+#cp $HOME/stash-theme.css .
 cp /run/media/jo-shva/Joshs_Slave/stash-theme.css .
-cp $HOME/stash-theme-Dracula.css .
-cp $HOME/stash-theme_tabnavs.css .
+#cp $HOME/stash-theme-Dracula.css .
+#cp $HOME/stash-theme_tabnavs.css .
 
 git add --all
 git commit -m "Sync Stash CSS"
